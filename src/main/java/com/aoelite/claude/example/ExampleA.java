eff177aa268fdf1e987c69dcc6c329ce4aca01c1
AoElite <20812471+AoElite@users.noreply.github.com>
2024-03-20 19:48:01 -0400

    add an example

---
+++ src/main/java/com/aoelite/claude/example/ExampleA.java
@@ -0,0 +1,34 @@
+package com.aoelite.claude.example;
+
+import com.aoelite.claude.ClaudeClient;
+import com.aoelite.claude.api.ClaudeAPI;
+import com.aoelite.claude.data.ClaudeMessage;
+import com.aoelite.claude.data.ClaudeRequest;
+import com.aoelite.claude.data.ClaudeResponse;
+import com.aoelite.claude.data.types.ClaudeModel;
+import com.aoelite.claude.data.types.ClaudeRole;
+
+public class ExampleA {
+
+    public static void main(String[] args) {
+
+        ClaudeAPI client = new ClaudeClient("YOUR_API_KEY");
+
+        ClaudeResponse response = client.sendRequest(ClaudeRequest.builder()
+                .model(ClaudeModel.HAIKU)
+                .max_tokens(100)
+                .temperature(1)
+                .system("You are a AI math assistant")
+                .message(new ClaudeMessage(ClaudeRole.USER, "what's 2+2?"))
+                .build());
+
+        // response could be null if it timed out
+        if (response != null) {
+            for (String string : response.getContent()) {
+                System.out.println(string);
+            }
+        }
+
+    }
+
+}

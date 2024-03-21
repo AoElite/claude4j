771b6f1e90106d2360f8711736b55e8f27d83f8e
AoElite <20812471+AoElite@users.noreply.github.com>
2024-03-20 21:47:30 -0400

    make default temperature 0

--- src/main/java/com/aoelite/claude/data/ClaudeRequest.java
+++ src/main/java/com/aoelite/claude/data/ClaudeRequest.java
@@ -16,7 +16,7 @@ public class ClaudeRequest {
     @Builder.Default private final int max_tokens = 1000;
     @Nullable private String system;
     @Nullable @Singular private List<String> stop_sequences;
-    @Builder.Default private double temperature = 1.0;
+    @Builder.Default private double temperature = 0.0;
     @NonNull @Singular private final List<ClaudeMessage> messages;
 
 }
